473b96c44879b7db203759553bc4c642bd09024a
Hessam <hessam.imen@gmail.com>
2023-12-13 09:19:10 +1100

    feat: create main routes

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,5 @@
+function MealDetails() {
+  return <h1>Meal Details</h1>;
+}
+
+export default MealDetails;
---
+++ app/meals/page.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+function MealsPage() {
+  return (
+    <>
+      <h1>Meals Pge</h1>
+      <h2>Links:</h2>
+      <p>
+        <Link href="/meals/share">SHARE</Link>{" "}
+      </p>
+      <p>
+        <Link href="/meals/community">COMMUNITY</Link>{" "}
+      </p>
+      <p>
+        <Link href="/meals/meal-details">MealDetails</Link>{" "}
+      </p>
+    </>
+  );
+}
+
+export default MealsPage;
