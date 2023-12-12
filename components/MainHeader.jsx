ae6159477a2d911d3955b5bb1467ac8ad34cc286
Hessam <hessam.imen@gmail.com>
2023-12-13 09:55:50 +1100

    style: the Main Header component is styled

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
+import classes from "./styles.module.css";
 
 import logoImg from "@/assets/logo.png"; //wehn importing image like this it returns an object that we should use the src key in src props
 
 function MainHeader() {
-  console.log(logoImg.src);
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
         <img src={logoImg.src} alt="A plate with food on it" />
         Next Level Food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Brouwse Meals</Link>
