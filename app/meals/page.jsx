7f358fbb7a96c450b2363b90dfd97f95b52ee7cc
Hessam <hessam.imen@gmail.com>
2023-12-13 22:44:30 +1100

    feat: implement the meals grid and meal item components

---
+++ app/meals/page.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose you favorite recipe and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
+}
+
+export default MealsPage;
