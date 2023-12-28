3460eed0b90e828d45128c3157921c35f3fee5bc
Hessam <hessam.imen@gmail.com>
2023-12-28 11:16:05 +1100

    feat: add server side input validatin

---
+++ app/meals/share/error.jsx
@@ -0,0 +1,10 @@
+"use client";
+function Error() {
+  return (
+    <main className="error">
+      <h1>An error occured</h1>
+      <p>Failed to create meal.</p>
+    </main>
+  );
+}
+export default Error;
